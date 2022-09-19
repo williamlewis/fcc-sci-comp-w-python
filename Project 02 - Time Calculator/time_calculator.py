ea4e8f85368b962351babf7c1ec18ea2d5073686
William Lewis <31929244+williamlewis@users.noreply.github.com>
2022-09-19 16:04:41 -0400

    Update time_calculator.py

--- Project 02 - Time Calculator/time_calculator.py
+++ Project 02 - Time Calculator/time_calculator.py
@@ -10,41 +10,57 @@
 
 
 
-#### -------- TEST INPUTS --------
 # test_input = ["11:30 AM", "2:32", "Monday"]
+from re import A
+
+
 test_input = ["6:30 PM", "205:12"]
 
 
 
-#### -------- STARTING VARIABLES --------
+days_of_wk = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+
 start = test_input[0]
 duration = test_input[1]
 day = None
+days_later = 0
+day_index = 9 # make this a cleaner solution
 if len(test_input) > 2:
-    day = test_input[2] '''update to correct / reformat capitalizatio'''
+    day = test_input[2]
+    day = day.lower()
+    day = day.capitalize()
+    day_index = days_of_wk.index(day)
 
-days_of_wk = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
 
+def check_days_ahead(res_hrs): # for res_hrs over 24, checks for number of full days forward and remaining hours
+    days_later = res_hrs // 24
+    extra_hrs = res_hrs % 24
 
-#### -------- FUNCTIONS --------
-# make small function to change between AM & PM
-def change_ampm(start_val):
-    if start_val == 'AM':
-        new_val = 'PM'
-        new_day = False
-    elif start_val == 'PM':
-        new_val = 'AM'
-        new_day = True
+    return days_later, extra_hrs    
+
+def change_ampm(start_ampm): # for res_hrs between 12 and 24; changes between AM & PM, then flags if time rolls into next day
+    if start_ampm == 'AM':
+        res_ampm = 'PM'
+        add_day = False
+    elif start_ampm == 'PM':
+        res_ampm = 'AM'
+        add_day = True
     else:
         print('Error changing b/w AM and PM')
-    return new_val, new_day
+    return res_ampm, add_day
 
 
-#----------------
+# def change_day(day):
+#     day_index = days_of_wk.index(day)
+#     if day_index < 7:
+#         day_index += 1
+#     elif day_index
+
+
+    day = days_of_wk[(index(day) + 1)]
 
 
 
-#### -------- TIME CALCULATIONS --------
 # break start time and duration inputs into component parts
 start_hrs = int(start.split(':')[0])
 start_mins = int(start.split(':')[1][:2])
@@ -59,6 +75,8 @@ dur_mins = int(duration.split(':')[1])
 res_hrs = start_hrs + dur_hrs
 res_min = start_mins + dur_mins
 res_ampm = start_ampm # set as original, to remain unless changed
+res_day = start_day # set as original, to remain unless changed
+
 
 
 # make time conversions to handle spilling over into next hour, into next AM/PM cycle, into next day, or into subsequent days
@@ -70,31 +88,102 @@ if res_min > 59:
     res_hrs += 1
 
 
+if res_hrs > 24:
+    while res_hrs > 24:
+        days_later += res_hrs // 24
+        day_index += days_later
+
+        extra_hours = res_hrs % 24
+        res_hrs = start_hrs + extra_hours
+
+if 12 < res_hrs < 24:
+    res_hrs -= 12
+    res_ampm, add_day = change_ampm(start_ampm)
+
+    if add_day is True:
+        days_later += 1
+        day_index += 1
+
+if res_hrs <= 12:
+    res_time = str(res_hrs) + ':' + str(res_min) + ' ' + res_ampm
+
+if day is None:
+    if days_later == 0:
+        A
+    elif days_later > 0:
+        A
+    else:
+        print('error calculating days later WITHOUT day of week')
+elif day is not None:
+    if days_later == 0:
+        A
+    elif days_later > 0:
+        A
+    else:
+        print('error calculating days later WITH day of week')
+else:
+    print('error calculating day')
 
 
-print(start)
-print(res_hrs)
+output = res_time # PLUS specific formatting
+    
+    print(res_time)
+    print(str(days_later) + ' days later')
+    # print(str(extra_hrs) + ' extra hours')
+    print(day_index)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
 # handle roll over to NEXT day (not multiple days), using change_ampm function
 if (res_hrs > 12) and (res_hrs < 24):
     res_hrs -= 12
     res_ampm, add_day = change_ampm(start_ampm)
 
     if add_day == True:
-        print('trigger day change here')
+        day_index += 1
 
 # handle roll over for MULTIPLE days ahead, also using change_ampm function
 elif res_hrs > 24:
     days_later = res_hrs // 24
     extra_hrs = res_hrs % 24
+    res_hrs = start_hrs + extra_hrs
+    
+    if res_hrs > 24:
+        days_later = res_hrs // 24
+        extra_hrs = res_hrs % 24
+        res_hrs = start_hrs + extra_hrs
 
-    res_hrs = start_hrs + extra_hrs # repeat mechanism here to check if res hours are STILL over 24, AFTER adding the extra hours
-    if (res_hrs > 12) and (res_hrs < 24):
+    
+    elif (res_hrs > 12) and (res_hrs < 24):
         res_hrs -= 12
         res_ampm, add_day = change_ampm(start_ampm)
 
         if add_day == True:
             days_later += 1
-            print('trigger day change here')
+            day_index += days_later
+    else:
+        print('problem trying to work hours down to 12 hours')
 
 
 else:
@@ -105,42 +194,10 @@ else:
     # break down in to 24hr cycles, to get # days later
     # add extra hours to convert to am/pm
     # still check for next day out of am/pm function, and add an ADDITIONAL day to # days later if it goes from PM to AM
+'''
 
 
 
-
-
-#### -------- FORMATTING & SOLUTION OUTPUT --------
-
-
-
-res_time = str(res_hrs) + ':' + str(res_min) + ' ' + res_ampm
-
-# print(start_hrs)
-# print(start_mins)
-# print(start_ampm)
-# print(day)
-# print(dur_hrs)
-# print(dur_mins)
-# print(res_hrs)
-# print(res_min)
-print(res_time)
-print(str(days_later) + ' days later')
-print(str(extra_hrs) + ' extra hours')
-
-
-# start_ampm = ''
-# start_hrs = ''
-# start_mins = ''
-# start_day = ''
-
-# dur_hrs = ''
-# dur_mins = ''
-
-# res_hrs = ''
-# res_mins = ''
-# res_ampm = ''
-# res_day = ''
 
 
 
@@ -179,6 +236,16 @@ def check_am_pm():
 
 def check_day_rollover:
 - for PM to AM conversion, trigger next day
+
+
+
+
+
+
+
+
+
+
 
 
 
