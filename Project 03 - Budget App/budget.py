964943d182ebb9ef2670c346d9b617b6a63ccab4
William Lewis <31929244+williamlewis@users.noreply.github.com>
2022-09-20 18:57:55 -0400

    Resolve category spend percentage rounding

--- Project 03 - Budget App/budget.py
+++ Project 03 - Budget App/budget.py
@@ -1,3 +1,4 @@
+import math
 
 class Category:
     def __init__(self, cat_name):
@@ -35,7 +36,7 @@ class Category:
         else:
             return False
     
-    # customize output when object is printed
+    # format output when object is printed
     def __str__(self):
 
         print_list = ''
@@ -50,22 +51,12 @@ class Category:
         for n in self.ledger:
             i = self.ledger.index(n)
             row_descr = self.ledger[i]['description'][:23]
-            # row_amount = str(round(float(self.ledger[i]['amount']), 2))
             row_amount = format(self.ledger[i]['amount'], '.2f')
 
-            # max_descr = self.ledger[i]['description'][:23]
-            # descr_leftover = ' ' * (descr_width - len(max_descr))
             descr_leftover = ' ' * (descr_width - len(row_descr))
-
-            # amount_leftover = ' ' * (amount_width - len(str(float(self.ledger[i]['amount']))))
-            # ledger_row = self.ledger[i]['description'][:23] + descr_leftover + amount_leftover + str(float(self.ledger[i]['amount'])) + '\n'
-            # amount_leftover = ' ' * (amount_width - len(str(round(float(self.ledger[i]['amount'])), 2)))
-            # ledger_row = self.ledger[i]['description'][:23] + descr_leftover + amount_leftover + str(round(float(self.ledger[i]['amount']), 2)) + '\n'
-            
             amount_leftover = ' ' * (amount_width - len(row_amount))
-            # ledger_row = self.ledger[i]['description'][:23] + descr_leftover + amount_leftover + str(round(float(self.ledger[i]['amount']), 2)) + '\n'
-            ledger_row = row_descr + descr_leftover + amount_leftover + row_amount + '\n'
-            
+
+            ledger_row = row_descr + descr_leftover + amount_leftover + row_amount + '\n'            
             print_list += ledger_row
 
         total_row = 'Total: ' + str(float(self.get_balance()))
@@ -76,4 +67,44 @@ class Category:
         return output
 
 
-# outside of Category class
+# build a simple bar chart to display categories by their percentage of overall spend (categories received as list argument)
+test_input = ['Clothing', 'Food', 'Auto']
+test_spending = {'Clothing': .2, 'Food': .6, 'Auto': .1}
+
+
+def create_spend_chart(categories):
+    categories = test_input
+
+    # check for 4 values max in argument
+    if len(categories) > 4:
+        categories = categories[:4]
+        print('Up to four categories allowed for bar chart; additional will not plot.')
+
+    # establish overall spend to calculate percentages
+    overall_spend = 0
+    for n in categories:
+        overall_spend += n.get_balance()
+    
+    # get balance & calculate rounded down percentage for each category
+    spend_percentages = []
+    for n in categories:
+        cat_spend = n.get_balance()
+        cat_perc = (cat_spend / overall_spend)
+        cat_perc_rounded = (math.floor(cat_perc * 10)) * 10 # make a float, then round down with math.floor(), then multiply by ten
+        spend_percentages.append([n, cat_perc_rounded, cat_perc])
+        # include cat_perc unrounded value to reference in case total sum of percentages does not equal 100?
+    
+    # # check that rounded category percentages total up to 100; if not, add 10 to highest raw value & repeat if needed
+    # sum_percentages = 0
+    # for n in spend_percentages:
+    #     sum_percentages += n[1]
+    # if sum_percentages > 100:
+    #     # for each cat, get diff between rounded & unrounded values (save into spend_percentages list as fourth item)
+    #     # make copy list & sort by rounded-unrounded difference values
+    #     delta = 100 - sum_percentages
+    #     num_corrections = delta / 10
+    #     for i in range(0, num_corrections):
+    #         # get category name from sorted copy list
+    #         # use category name to access sublist in main spend percentages list, cat_perc_rounded value item
+    #         # add 10 to cat_perc_rounded value item (directly in main spend_percengtages list; overwrite)
+    
