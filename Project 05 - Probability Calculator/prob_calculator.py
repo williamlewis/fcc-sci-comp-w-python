78c09e877f2d91822011ae59523ced0869207203
William Lewis <31929244+williamlewis@users.noreply.github.com>
2022-09-22 07:56:45 -0400

    Update prob_calculator.py

--- Project 05 - Probability Calculator/prob_calculator.py
+++ Project 05 - Probability Calculator/prob_calculator.py
@@ -10,23 +10,30 @@ class Hat:
             ball_num = value
             for i in range(0, ball_num):
                 self.contents.append(ball_col)
-        self.original_contents = copy.deepcopy(self.contents)
+        # self.original_contents = copy.deepcopy(self.contents)
     
     def draw(self, num_to_draw):
         if num_to_draw > len(self.contents):
-            #self.contents = self.original_contents
+            # self.contents = self.original_contents
             drawn_balls = self.contents
             return drawn_balls
         else:
-            indices_to_draw = []
-            for i in range(0, num_to_draw):
-                indices_to_draw.append(random.randint(0, len(self.contents)))
+            # indices_to_draw = []
+            # for i in range(0, num_to_draw):
+            #     indices_to_draw.append(random.randint(0, len(self.contents)))
             
-            drawn_balls = []
-            for i in indices_to_draw:
-                drawn_balls.append(self.contents[i])
-                self.contents.pop(i)
+            # drawn_balls = []
+            # for i in indices_to_draw:
+            #     drawn_balls.append(self.contents[i])
+            #     self.contents.pop(i)
             
+            drawn_balls = random.choices(self.contents, num_to_draw)
+            # drawn_ball_indices = []
+            # for n in drawn_balls:
+            #     drawn_ball_indices.append(drawn_balls.index(n))
+            for n in drawn_balls:
+                self.contents.pop(drawn_balls.index(n))
+                        
             return drawn_balls
 
 
