be064672cb874a18926969be29fe1f35368a41c1
William Lewis <31929244+williamlewis@users.noreply.github.com>
2022-09-22 18:22:01 -0400

    Probability Calculator final solution

--- Project 05 - Probability Calculator/prob_calculator.py
+++ Project 05 - Probability Calculator/prob_calculator.py
@@ -27,10 +27,10 @@ def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
     
     num_outcome_matches = 0 # incremented up only when full match condition (for multiple colors) is met within an experiment attempt
 
-    for i in range(0, num_experiments): # do correct number of experiments
-        # draw balls from hat copy
+    # draw balls from hat for correct number of experiments
+    for i in range(0, num_experiments):
         hat_exp = copy.deepcopy(hat) # make copy to start fresh in each experiment attempt
-        drawn_balls = hat_exp.draw(num_balls_drawn) # draw balls from hat_exp
+        drawn_balls = hat_exp.draw(num_balls_drawn)
         
         # convert drawn ball outcome from list to dictionary, to compare against expected_balls argument
         outcome = {}
@@ -40,23 +40,8 @@ def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
             else:
                 outcome[n] = 1
         
-        # for each expected color, check if experiment outcome included at least as many balls as expected
-        all_colors_match = []
-        # for key, val in expected_balls.items():
-        #     expected_col = key
-        #     expected_num = val
-
-        #     if expected_col in outcome:
-        #         if outcome[expected_col] >= expected_num:
-        #             all_colors_match = True
-        #         else:
-        #             all_colors_match = False
-        #     else:
-        #         all_colors_match = False
-
-        
-        
-        
+        # for each expected color, check if experiment outcome includes at least as many balls as expected
+        all_colors_match = [] # ensure that match is met for all colors within expected_balls
         
         for key, value in expected_balls.items():
             expected_col = key
@@ -67,32 +52,9 @@ def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
             else:
                 all_colors_match.append(False)
     
-        # only if all_colors_match bool was changed to True and remained true for all colors, increment num_outcome_matches
+        # if all colors in expected_balls were matched, increment total num_outcome_matches
         if all(all_colors_match):
             num_outcome_matches += 1
-
-
-
-
-        # outcome = {}
-        # for n in drawn_balls:
-        #     if n in outcome:
-        #         outcome[n] += 1
-        #     else:
-        #         outcome[n] = 1
-
-        # if outcome == expected_balls:
-        #     num_outcome_matches += 1
-
-
-        # for i in range(0, num_experiments): # FOR N IN LIST OF ALL OUTCOMES??
-        #     all_colors_match = False
-        #     for color in expected_balls:
-        #         if (color in outcome) and (outcome[color] >= expected_balls[color]):
-        #             all_colors_match = True
-        #         else:
-        #             all_colors_match = False
-        #     num_outcome_matches += 1
 
     probability = num_outcome_matches / num_experiments
 
